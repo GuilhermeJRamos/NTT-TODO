520ec40988f31bbaecc8a38a9abb1893ce411bc2
GuilhermeJRamos <guilhermedejesusramos23@gmail.com>
2024-03-18 15:22:01 -0300

    project inicial

---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+
+// Pages
+import { ListComponent } from './modules/todolist/pages/list/list.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: ListComponent,
+  }
+];
